5eee3402ba4d05dc6f607585ee8d3b98b08d543c
epich <devnull@localhost>
2012-08-10 19:08:36 -0400

    Starting byte compiling more Elisp.

--- rc/.emacs.d/emacsUtil.py
+++ rc/.emacs.d/emacsUtil.py
@@ -1,35 +1,61 @@
 #! /usr/bin/env python
 #
-# Something quick and dirty to make the third party Emacs code I use.
+# Utilities for managing Elisp.  Byte compiling primarily.
 
 import os
 import sys
+# Bootstrap generalUtil
+sys.path.append( '~/unihome/py' )
+import generalUtil
+
+evilDir_g = 'evil'
+cedetDir_g = 'cedet-1.1'
+jdeeDir_g = 'jdee-2.4.0.1'
+pareditDir_g = './paredit'
+
+def byteCompile(filesString, loadPathL=[]):
+   """Byte compile the inputted elisp files.
+
+   loadPathL -- list of load paths, passed to emacs -L option.  ie paths to dependencies.
+   filesString -- string which is passed to command line.  Example: '*.el'
+   """
+   loadPathOpts = ['-L '+loadPathI+' ' for loadPathI in loadPathL]
+   generalUtil.cmd( 'emacs --batch -Q %s -f batch-byte-compile %s'%(''.join(loadPathOpts), filesString,), printStdout=True )
 
 def buildEvil():
-   os.chdir('evil')
-   os.system('make')
-   os.chdir('..')
+   generalUtil.cmd('make -C %s'%(evilDir_g,), printStdout=True)
 
 def buildCedet():
    # CEDET contributes to annoying interrogations at Emacs closing time.
    # Create the .semanticdb dir to avoid one interrogation question.
-   os.system('mkdir -p ~/.semanticdb')
-   os.chdir('cedet-1.1')
+   generalUtil.cmd('mkdir -p ~/.semanticdb')
    # There was a case where cedet*/semantic/Makefile needed a tender touch.
    # Might as well touch 'em all.
-   os.system('touch `find . -maxdepth 2 -name Makefile`')
-   os.system('make')
+   generalUtil.cmd('touch `find %s -maxdepth 2 -name Makefile`'%(cedetDir_g,))
+   generalUtil.cmd('make -C %s'%(cedetDir_g,), printStdout=True)
 
 def buildJdee():
    # Assumes jde.el was patched to change jde-cedet-max-version .
-   patchedFilePath = 'jdee-2.4.0.1/lisp/jde.elc'
-   if os.path.isfile(patchedFilePath):
-      os.system('rm %s'%(patchedFilePath,))
+   jdeeLispDir = jdeeDir_g+'/lisp'
+   patchedFilePathNoExt = '%s/jde'%(jdeeLispDir,)
+   if os.path.isfile(patchedFilePathNoExt+'.elc'):
+      generalUtil.cmd('rm %s'%(patchedFilePathNoExt+'.elc',))
+   # TODO: Doesn't compile
+   #byteCompile(patchedFilePathNoExt+'.el', [cedetDir_g+'/semantic', jdeeLispDir,])
+
+def buildParedit():
+   byteCompile(pareditDir_g+'/paredit.el')
+
+def buildInitElisp():
+   # TODO: Runtime errors
+   pass#byteCompile('init.el', [evilDir_g, pareditDir_g, TODO,])
 
 def buildEmacsD():
    buildEvil()
    buildCedet()
    buildJdee()
+   buildParedit()
+   buildInitElisp()
 
 def __MAIN__():
    buildEmacsD()
