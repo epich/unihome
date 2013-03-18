497d92075048596453571b14e1dba4e0daea9867
epich <devnull@localhost>
2013-03-18 00:00:48 -0400

    More tweaks to CEDET.

--- rc/.emacs.d/init.el
+++ rc/.emacs.d/init.el
@@ -203,11 +203,11 @@ or nil if not found."
   "Loads the latest snapshot of CEDET bzr trunk. "
   (defvar my-cedet-path "/psd15/linux/boreilly/sw/cedet-bzr/trunk" "Path to CEDET")
   (load-file (format "%s/cedet-devel-load.el" my-cedet-path))
-  ;; require eassist
-  (load-file (format "%s/contrib/eassist.el" my-cedet-path))
+  (add-to-list 'load-path (concat my-cedet-path "contrib"))
   (require 'semantic/ia)
   (require 'semantic/bovine/gcc)
   (semantic-mode 1)
+  (require 'eassist)
   (global-ede-mode 1)
   )
 ;; For now, while trying out CEDET from bzr
@@ -541,6 +541,7 @@ nil in keymap-from."
 (define-key evil-motion-state-map "F" 'other-window)
 (define-key evil-normal-state-map "-" nil)
 (define-key evil-motion-state-map "-" 'evil-end-of-line)
+;; TODO: Get used to Emacs defaults M-. and M-* , then delete q and Q bindings.
 (define-key evil-normal-state-map "q" 'find-tag)
 (define-key evil-motion-state-map "Q" 'pop-tag-mark)
 (define-key evil-normal-state-map "s" nil)
