1e4fd1417ff879db549cbe7659e3fd28d824d2b2
epich <devnull@localhost>
2013-12-11 18:55:17 -0500

    Gut out cp-propertize-region-* in favor of the winner. Other color-parens cleanups.

--- emacs/lisp/color-parens.el
+++ emacs/lisp/color-parens.el
@@ -158,116 +158,6 @@ POSITIONS is a list of positions in the buffer to colorize."
                                   rear-nonsticky
                                   nil))))))
 
-(defsubst color-parens--update-inconsistency-colors (inconsistentp
-                                                     open-paren
-                                                     close-paren)
-  "Update inconsistency Font Lock colors for OPEN-PAREN and
-CLOSE-PAREN as buffer positions based on INCONSISTENTP."
-  (if inconsistentp
-      (color-parens--colorize (list open-paren close-paren)
-                              'cp-inconsistent)
-    (color-parens--decolorize (list open-paren close-paren))))
-
-(defun cp-propertize-region-3 (start end)
-  (save-excursion
-    (goto-char start)
-    (let ((paren-stack nil)
-          (line-ppss (syntax-ppss)))
-      (dolist (open-pos (nth 9 line-ppss))
-        ;; TODO: Initialize :inconsistent
-        (push (make-cp--Open :position open-pos
-                                       :column (save-excursion
-                                                 (goto-char open-pos)
-                                                 (current-column)))
-              paren-stack))
-      (while (< (point) end)
-        (let (;; Column at which text starts on the line, except if
-              ;; inside a string. Text doesn't start in a comment,
-              ;; since ; is text.
-              (text-column (progn (back-to-indentation)
-                                  (current-column)))
-              (line-end (save-excursion (end-of-line)
-                                        (point))))
-          ;; Skip whitespace only lines and lines beginning inside
-          ;; string
-          (unless (or (eq (point) line-end)
-                      (nth 3 line-ppss)) ; Whether inside string
-            ;; Mark open parens on the paren-stack that become
-            ;; inconsistent because of the current line.
-            (let ((open-i paren-stack))
-              ;; If one considers only the inconsistent==nil Opens on
-              ;; the paren-stack, their columns are strictly
-              ;; decreasing moving down the stack (towards the tail).
-              ;; Since we're only interested in marking Opens
-              ;; inconsistent, that allows the iteration to stop at
-              ;; the first inconsistent==nil Open with small enough
-              ;; column.
-              (while (and open-i
-                          (or (cp--Open-inconsistent (car open-i))
-                              (<= text-column
-                                  (cp--Open-column (car open-i)))))
-                (setf (cp--Open-inconsistent (car open-i))
-                      t)
-                (pop open-i))))
-          ;; TODO: Let bound current point and sexp depth here so as
-          ;; scan-lists can use it to efficiently find the close
-          ;; parens we're passing over.
-          ;;
-          ;; Go to next line. Since we already know line-end, use it
-          ;; instead of rescanning the line
-          (goto-char (min (1+ line-end) (point-max)))
-          (setq line-ppss (syntax-ppss))
-          (let* ((open-positions (nth 9 line-ppss))
-                 (common-open (and paren-stack
-                                  (let ((cons-i open-positions))
-                                    (while (and (cdr cons-i)
-                                                (<= (cadr cons-i)
-                                                    (cp--Open-position (car paren-stack))))
-                                      (pop cons-i))
-                                    cons-i))))
-            ;; Process parens that closed upon going to this next line
-            (while (and paren-stack
-                        (or (not common-open)
-                            (/= (cp--Open-position (car paren-stack))
-                                (car common-open))))
-              (let ((close-pos (condition-case nil
-                                   (1- (scan-lists (cp--Open-position (car paren-stack))
-                                                   1 0))
-                                 (scan-error nil)))
-                    (open-obj (pop paren-stack)))
-                ;; TODO: Set mismatched paren face if close-pos is nil
-                ;; TODO: If operating on a subset region, it's not
-                ;; always correct to color consistent
-                (color-parens--update-inconsistency-colors
-                 (cp--Open-inconsistent open-obj)
-                 (cp--Open-position open-obj)
-                 close-pos)))
-            ;; Create new cp--Open objects
-            (save-excursion
-              (dolist (open-i (if common-open
-                                  (cdr common-open)
-                                open-positions))
-                (goto-char open-i)
-                ;; TODO: Account for the case of skipping lines that
-                ;; begin in a string. In those cases, these parens
-                ;; we're adding could be inconsistent amongst
-                ;; themselves in a way that the algorithm won't detect
-                ;; because of the short circuiting of the paren-stack
-                ;; check. Either do a particular consistency check
-                ;; here, or add cp--Open objects on lines
-                ;; that begin in a string (leaning towards former).
-                (push (make-cp--Open :position open-i
-                                               :column (current-column))
-                      paren-stack))))))
-      (dolist (open-i paren-stack)
-        (color-parens--update-inconsistency-colors
-         (cp--Open-inconsistent open-i)
-         (cp--Open-position open-i)
-         (condition-case nil
-             (1- (scan-lists (cp--Open-position open-i)
-                             1 0))
-           (scan-error nil)))))))
-
 (defsubst cp--line-check-opens (open-stack)
   "Check cp--Open objects of the OPEN-STACK list for
 consistency.
@@ -368,7 +258,7 @@ INIT-POS should arrive at its close position."
 ;; see the effect on performance. On the one hand, it doesn't benefit
 ;; from cp--set-closes function's optimization, but could benefit from
 ;; less garbage collection.
-(defun cp-propertize-region-2 (start end)
+(defun cp-propertize-region (start end)
   (save-excursion
     (let* ((timing-info (list (current-time)))
            (start-ps (syntax-ppss start))
@@ -434,7 +324,6 @@ INIT-POS should arrive at its close position."
                                       (progn
                                         (push (make-cp--Open
                                                :position open-pos
-                                               ;; TODO: move init of close to here
                                                :column (progn
                                                          (goto-char open-pos)
                                                          (current-column)))
@@ -479,148 +368,41 @@ INIT-POS should arrive at its close position."
         (push (current-time) timing-info)
         (dolist (open-i open-objs)
           ;; Set close position
+          ;;
+          ;; Note: We do it here instead of when it was made so as
+          ;; some benefit from the cp--set-closes function's buffer
+          ;; scanning optimization. The lists processed here are
+          ;; opened and closed within JIT lock's region, so the less
+          ;; efficient buffer scanning is not a big deal.
           (unless (cp--Open-close open-i)
             (setf (cp--Open-close open-i)
                   (condition-case nil
                       (1- (scan-lists (cp--Open-position open-i) 1 0))
+                    ;; TODO: Set to 'mismatched
                     (scan-error nil))))
           (if (cp--Open-inconsistent open-i)
               (color-parens--colorize (list (cp--Open-position open-i)
                                             (cp--Open-close open-i))
                                       'cp-inconsistent)
-            ;; TODO: Until we process parens after end of region, check
-            ;; its within
-            (when (< (cp--Open-close open-i) end)
-              (color-parens--decolorize (list (cp--Open-position open-i)
-                                              (cp--Open-close open-i))))))
+            (color-parens--decolorize (list (cp--Open-position open-i)
+                                            (cp--Open-close open-i)))))
         (push (current-time) timing-info)
         (my-msg "DEBUG: cp-color-parens start=%s end=%s timing: %s"
                 start end
                 (my-time-diffs (nreverse timing-info)))
         ))))
 
-(defun cp-propertize-region-1 (start end)
-  (save-excursion
-    (goto-char start)
-    (beginning-of-line)
-    (let (;; Push at open parens, pop at close parens
-          (paren-stack)
-          (parse-state (syntax-ppss)))
-      (while (< (point) end)
-        (let (;; Column at which text starts on the line, except if
-              ;; inside a string. Text doesn't start in a comment,
-              ;; since ; is text.
-              (text-column (progn (back-to-indentation)
-                                  (current-column)))
-              (line-end (save-excursion (end-of-line)
-                                        (point))))
-          ;; Skip whitespace only lines
-          (unless (eq (point) line-end)
-            (unless (nth 3 parse-state) ; Whether inside string
-              ;; Mark open parens on the paren-stack that become
-              ;; inconsistent because of the current line.
-              (let ((open-i paren-stack))
-                ;; If one considers only the inconsistent==nil Opens on
-                ;; the paren-stack, their columns are strictly
-                ;; decreasing moving down the stack (towards the tail).
-                ;; Since we're only interested in marking Opens
-                ;; inconsistent, that allows the iteration to stop at
-                ;; the first inconsistent==nil Open with small enough
-                ;; column.
-                (while (and open-i
-                            (or (cp--Open-inconsistent (car open-i))
-                                (<= text-column
-                                    (or (cp--Open-column (car open-i))
-                                        ;; Lazy computation of column
-                                        (save-excursion
-                                          (goto-char (cp--Open-position (car open-i)))
-                                          (setf (cp--Open-column (car open-i)) (current-column)))))))
-                  (setf (cp--Open-inconsistent (car open-i))
-                        t)
-                  (pop open-i))))
-            ;; Note: point is at indentation
-            (while (and (< (point) line-end))
-              (let ((depth-change
-                     (- (car parse-state)
-                        (car (setq parse-state
-                                   ;; TODO: parsing 1 char at a time
-                                   ;; is too slow.
-                                   ;; (parse-partial-sexp (point)
-                                   ;;                     (1+ (point))
-                                   ;;                     nil
-                                   ;;                     nil
-                                   ;;                     parse-state
-                                   ;;                     nil)
-                                   ;; Improves speed with 666666 hack
-                                   ;; (stop at any depth change)
-                                   (parse-partial-sexp (point)
-                                                       line-end
-                                                       666666
-                                                       nil
-                                                       parse-state
-                                                       'syntax-table)
-                                   )))))
-                (cond
-                 ((or (= 0 depth-change)   ; Didn't cross a paren
-                      (nth 3 parse-state)  ; Inside a string
-                      (nth 4 parse-state)) ; Inside a comment
-                  nil) ; Keep parsing
-                 ;; Case: stopped at open paren
-                 ((< depth-change 0)
-                  ;; Note: cp--Open's column field is
-                  ;; initialized nil and computed lazily. This avoids
-                  ;; computing current-column for open parens closed
-                  ;; on the same line. These also tend to be further
-                  ;; from the beginning of line.
-                  (push (make-cp--Open :position (1- (point)))
-                        paren-stack))
-                 ;; Case: stopped at close paren
-                 ((< 0 depth-change)
-                  (if paren-stack
-                      (progn
-                        (color-parens--update-inconsistency-colors
-                         (cp--Open-inconsistent (car paren-stack))
-                         (cp--Open-position (car paren-stack))
-                         (1- (point)))
-                        (pop paren-stack))
-                    ;; TODO: Handle close paren when nil paren-stack
-                    ))))))
-          ;; Go forward to beginning of next line, keeping parse-state
-          ;; up to date
-          (unless (eobp)
-            (setq parse-state
-                  (parse-partial-sexp (point)
-                                      (1+ (point))
-                                      nil
-                                      nil
-                                      parse-state))))))))
-
 (defun color-parens-unpropertize-region (start end)
   ;; TODO: remove-text-properties
   )
 
-(defun color-parens-extend-region (start end)
-  "Extend region for JIT lock to fontify."
-  (save-excursion
-    (list (or (syntax-ppss-toplevel-pos (syntax-ppss start))
-              start)
-          (let ((last-top-level (syntax-ppss-toplevel-pos (syntax-ppss end))))
-            (if last-top-level
-                (progn
-                  (goto-char last-top-level)
-                  (forward-sexp)
-                  (point))
-              end)))))
-
-;; TODO: Extend to either outermost list or visible range? Consider
-;; how the code will know it can change inconsistent to consistent if
-;; not processing an expanded region.
 (defsubst color-parens-extend-region-after-change (start end _old-len)
-  (let ((extended-region (color-parens-extend-region start end)))
-    (setq jit-lock-start (car extended-region))
-    (setq jit-lock-end (cadr extended-region))))
-
-(defalias 'cp-propertize-region 'cp-propertize-region-2)
+  ;; It seems redisplay works its way from before start to after end,
+  ;; so it's more important to expand the start in order to get
+  ;; correct redisplays.
+  ;; TODO: Account for cp-inconsistency-max-lines
+  (setq jit-lock-start (or (syntax-ppss-toplevel-pos (syntax-ppss start))
+                           start)))
 
 (define-minor-mode color-parens-mode
   "Color unbalanced parentheses and parentheses inconsistent with
@@ -628,13 +410,11 @@ INIT-POS should arrive at its close position."
   nil nil nil
   (if color-parens-mode
       (progn
-        (jit-lock-register 'cp-propertize-region
-                           t)
+        (jit-lock-register 'cp-propertize-region t)
         (add-hook 'jit-lock-after-change-extend-region-functions
                   'color-parens-extend-region-after-change
                   nil
-                  t)
-        )
+                  t))
     (jit-lock-unregister 'cp-propertize-region)
     (color-parens-unpropertize-region (point-min) (point-max))))
 
